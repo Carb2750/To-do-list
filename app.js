6c2ea3995a84cd00bb6c3cbbcaa3cdbf0679b021
Carb2750 <carb2750@hotmail.com>
2019-01-08 03:00:00 -0600

    Add deleteAll function

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ new Vue({
             if(data != ""){
                 this.list.push(data);
                 this.text = ""; 
+                this.$refs.inputList.focus();
             }
         },
         removeItem: function(id){
             this.list.splice(id, 1);
+        },
+        deleteAll: function(){
+            var listSize = this.list.length;
+            for(var i = 0; i < listSize; i++){
+                this.list.pop();
+            }
         }
     }
 })
