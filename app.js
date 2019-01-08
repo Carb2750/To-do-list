5d2ef52b371ad7dd8b64d43089aae447ca6a7967
Carb2750 <carb2750@hotmail.com>
2019-01-08 01:41:32 -0600

    Add addItem and removeItem Methods

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 new Vue({
     el: '#app',
     data: {
-        
+        list: [],
+        text: ''
+    },
+    methods: {
+        addItem: function(data){
+            if(data != ""){
+                this.list.push(data);
+                this.text = ""; 
+            }
+        },
+        removeItem: function(id){
+            this.list.splice(id, 1);
+        }
     }
 })
